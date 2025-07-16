704fd3958b1877e36f2bbae84d5d3d4758d12191
PJavorek89 <pavel.javorek89@gmail.com>
2025-07-16 23:48:07 +0200

    add documentation for filterMovie fce + change for movie with no img in filterMovie.css

--- src/function/filterMovie.ts
+++ src/function/filterMovie.ts
@@ -1,13 +1,17 @@
-//import addElement from "./addElement";
-
 import addElement from "./addElement";
 import "/src/css/filterMovie.css";
 
+/**
+ * Interface of image type of filtered movie
+ */
 interface ImageType {
   medium: string;
   original: string;
 }
 
+/**
+ * Interface of filtered movie
+ */
 interface Movie {
   score: number;
   show: {
@@ -16,11 +20,16 @@ interface Movie {
   };
 }
 
-const addMovieFilter = (idOfElement: string) => {
+/**
+ *  function, which will render image of filtered movie
+ *
+ * @param idOfElement element, where on the page will be added filtered movie
+ */
+const addMovieFilter = (idOfElement: string): void => {
   const selectMovieElement = document.getElementById(
     idOfElement
   ) as HTMLSelectElement | null;
-  const spaceForFilteredMovies = document.createElement("div") as HTMLElement;
+  const spaceForFilteredMovies = document.createElement("div");
   spaceForFilteredMovies.classList.add("filtered-movie-spaceholder");
   selectMovieElement?.insertAdjacentElement(
     "afterend",
@@ -34,16 +43,23 @@ const addMovieFilter = (idOfElement: string) => {
   });
 };
 
+/**
+ * Fetch data and render them to page or render error element
+ * @param valueToFilter value of filter, which will be send to API
+ * @param whereToAdd element, where will be filtered movie (or error) added
+ */
 const filteredMovie = async (
   valueToFilter: string,
   whereToAdd: HTMLElement
 ) => {
   const urlToCall = "https://api.tvmaze.com/search/shows?q=";
-  let data: [] = [];
+  let data: Movie[] = [];
+  //occurrance of error false=no error, true = error
   let isError: boolean = false;
+  //value of error type for error rendering
   let errorType: Text = document.createTextNode("");
-  //const isLoading: boolean = true;
-  //vytvorit loadinf element a dat do dat (ne, vyresit pripnuti a odepnuti)
+
+  //create loading element and add him to page in the start the filteredMovie function
   const loadingElement = addElement({
     typeOfElement: "p",
     classToAdd: "loading-element",
@@ -52,39 +68,34 @@ const filteredMovie = async (
     idToAdd: "loading-element",
   });
   loadingElement.innerText = "Loading...";
-  //vytvorit error element a pridat do dat v pripade
+
+  //create error element for render in case of error occurrence
   const errorElement = addElement({
     typeOfElement: "div",
     classToAdd: "error-element",
     idToAdd: "error-element",
   });
   errorElement.textContent = "Opps, something went wrong \n";
+
+  //fetch data or catch error element in case of error
   try {
     const response = await fetch(urlToCall + valueToFilter);
     data = await response.json();
     if (!response.ok) {
-      throw new Error(`${response.status.toString()}`);
+      throw new Error(`${response.status}`);
     }
   } catch (err) {
     console.log("catch" + err);
     isError = true;
     errorType = document.createTextNode(err as string);
-    //console.log(errorType);
-
-    //whereToAdd?.appendChild(errorElement);
   } finally {
     whereToAdd?.removeChild(loadingElement);
   }
 
-  //na konci cele fetch jen vrati data
-  console.log(isError);
-
-  //mozna if-else i na error (true, false)
-
+  //render error or movie to the page
   if (isError) {
     errorElement.appendChild(errorType);
     whereToAdd?.appendChild(errorElement);
-    console.log("isError -> true");
   } else {
     data.forEach((movie: Movie) => {
       const { image, name } = movie.show;
@@ -101,8 +112,6 @@ const filteredMovie = async (
           whereToAdd: movieFilterEnvelope,
         });
         noImageTag.textContent = "(no image)";
-
-        return true;
       } else {
         const { medium } = image;
         const movieFilterEnvelope = addElement({
@@ -116,30 +125,9 @@ const filteredMovie = async (
         }) as HTMLImageElement;
         imgElement.src = medium;
         imgElement.alt = `image for movie ${name}`;
-        return medium;
       }
     });
   }
-
-  //console.log(newArray);
 };
-
-/*
-const valueForm = document.getElementById("movie-form");
-console.log(valueForm);
-valueForm?.addEventListener("change", (e) => {
-  //let testLog = "";
-  const target = e.target as HTMLOptionElement;
-  console.log("change");
-  console.log(`select: ${target.value}`);
-
-  const urlToCall = "https://api.tvmaze.com/search/shows?q=";
-  const valueToFilter = target.value;
-
-  console.log(`target URL: ${urlToCall + valueToFilter}`);
-
-  filteredMovie(valueToFilter);
-});
-*/
 
 export default addMovieFilter;
